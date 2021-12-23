038603c5f1beb247e47de4945c29212f96a6a0d5
Andrei Orlov <andrei_orlov2@epam.com>
2021-12-23 23:58:10 +0700

    add frontend part of project

--- Elaquak/src/constants/apiPaths.ts
+++ Elaquak/src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://g7kdbhhx2l.execute-api.eu-north-1.amazonaws.com/dev",
+  product: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://g7kdbhhx2l.execute-api.eu-north-1.amazonaws.com/dev",
   import: "https://g7kdbhhx2l.execute-api.eu-north-1.amazonaws.com/dev",
-  bff: "https://g7kdbhhx2l.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://g7kdbhhx2l.execute-api.eu-north-1.amazonaws.com/dev",
 };
 
