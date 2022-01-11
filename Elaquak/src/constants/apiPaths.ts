e087ed6c619c1ce9fe9c4958877d771b439bca67
Andrei Orlov <andrei_orlov2@epam.com>
2022-01-11 20:48:40 +0700

    fix bff paths

--- Elaquak/src/constants/apiPaths.ts
+++ Elaquak/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://ph9rvpqhy9.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
